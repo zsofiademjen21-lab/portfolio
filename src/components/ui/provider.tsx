c83c44ce4ab89849d7dae13baafc1f06ff530348
Zsófia Demjén-Nagy <zsofia.demjen21@gmail.com>
2026-09-04 13:12:24 +0200

    chakra ui init

---
+++ src/components/ui/provider.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+
+export function Provider({ children }: { children: React.ReactNode }) {
+  return <ChakraProvider value={defaultSystem}>{children}</ChakraProvider>;
+}
